10599921319bc9514a9709d01442bd9412c32091
Ivn-Cdrc <ivan.cedric.bulosan@gmail.com>
2024-02-02 07:55:23 +1100

    Added spring boot backend

---
+++ src/main/java/com/potoroo/demo/PotorooApplication.java
@@ -0,0 +1,13 @@
+package com.potoroo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotorooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotorooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/potoroo/demo/PotorooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.potoroo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotorooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
